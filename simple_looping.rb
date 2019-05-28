4f8a6f3b80532f49a223f99ed5609d4ce97c4193
Kelliann Lafferty <kell10192@gmail.com>
2019-05-28 21:58:34 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,8 +2,12 @@
 
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0
   loop do 
+    counter = counter + 1 
     puts phrase 
+    if counter >= 7 
+      break
   end
 end
 
